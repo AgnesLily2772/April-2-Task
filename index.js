b60099ca0e1922157ad1fc99a26d15bb4af8449f
AGNES LILY <97900099+AgnesLily2772@users.noreply.github.com>
2022-06-23 14:16:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ console.log(resume)
 
 const pageSetup = document.createElement("div");
 pageSetup.className = "container mydiv";
+const isLength = (val) =>{
+  let cnt = 0;
+  //for loop
+  for(let p=0;p<val.length;p++)
+  {
+    cnt++;
+  }
+  if(cnt === val.length)
+  {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 let ResumeDetails,ResumeElement,ResumeCategory,ResumeDiv,ResumeAligh;
+//for in loop
 for(let i in resume)
 { 
     const detail = document.createElement("div");
@@ -11,12 +27,14 @@ for(let i in resume)
     ResumeDetails = document.createElement("h3");
     ResumeDetails.innerText = i;
     detail.append(ResumeDetails);
-    if(Array.isArray(resume[i]))
+    if(Array.isArray(resume[i]) && isLength(resume[i]))
     {
       ResumeDiv = document.createElement("div");
       ResumeDiv.className="idiv";
+      //foEach loop
             resume[i].forEach((k=>{
-                for(let key in k)
+              //for of loop
+                for(let [key,value] of Object.entries(k))
                 {
                     ResumeAligh = document.createElement("tr");
                     ResumeAligh.className = "mytr";
